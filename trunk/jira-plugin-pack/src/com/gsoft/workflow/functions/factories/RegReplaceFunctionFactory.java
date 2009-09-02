71edfcf8b10550a8368f9c276a7aa519574bee88
Ganzha.Vitaliy <Ganzha.Vitaliy@b272e688-8835-11de-a4f2-b7219ab9ea91>
2009-09-02 20:28:51 +0000

    Added: Regreplace function

--- trunk/jira-plugin-pack/src/com/gsoft/workflow/functions/factories/RegReplaceFunctionFactory.java
+++ trunk/jira-plugin-pack/src/com/gsoft/workflow/functions/factories/RegReplaceFunctionFactory.java
@@ -1,28 +1,29 @@
-package com.gsoft.workflow.validators.factories;
+package com.gsoft.workflow.functions.factories;
 
 import com.atlassian.jira.issue.fields.Field;
 import com.atlassian.jira.plugin.workflow.AbstractWorkflowPluginFactory;
-import com.atlassian.jira.plugin.workflow.WorkflowPluginValidatorFactory;
+import com.atlassian.jira.plugin.workflow.WorkflowPluginFunctionFactory;
 import com.googlecode.jsu.util.CommonPluginUtils;
 import com.googlecode.jsu.util.WorkflowUtils;
 import com.opensymphony.workflow.loader.AbstractDescriptor;
-import com.opensymphony.workflow.loader.ValidatorDescriptor;
+import com.opensymphony.workflow.loader.FunctionDescriptor;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 /**
+ * @author User
  *
- * @author Ganzha Vitaliy
+ * $Rev$
  */
-public class RegularExpressionValidatorFactory extends AbstractWorkflowPluginFactory
-		implements WorkflowPluginValidatorFactory{
+public class RegReplaceFunctionFactory extends AbstractWorkflowPluginFactory implements
+        WorkflowPluginFunctionFactory{
 
     @Override
-    protected void getVelocityParamsForInput(Map velocityParams) {
+    protected void getVelocityParamsForInput(Map velocityParams)
+    {
         List<Field> fields = CommonPluginUtils.getAllEditableFields();
         velocityParams.put("fieldsList", fields);
-        
     }
 
     @Override
@@ -32,20 +33,26 @@ public class RegularExpressionValidatorFactory extends AbstractWorkflowPluginFac
     }
 
     @Override
-    protected void getVelocityParamsForView(Map velocityParams, AbstractDescriptor descriptor) {
-        ValidatorDescriptor validatorDescriptor = (ValidatorDescriptor) descriptor;
-        String fieldKey = (String) validatorDescriptor.getArgs().get("selectedField");
+    protected void getVelocityParamsForView(Map velocityParams, AbstractDescriptor descriptor)
+    {
+        FunctionDescriptor functionDescriptor = (FunctionDescriptor) descriptor;
+        String fieldKey = (String) functionDescriptor.getArgs().get("selectedField");
         velocityParams.put("selectedField", WorkflowUtils.getFieldFromKey(fieldKey));
-        velocityParams.put("pattern", validatorDescriptor.getArgs().get("patternEdit"));
+        velocityParams.put("pattern", functionDescriptor.getArgs().get("patternEdit"));
+        velocityParams.put("replacement", functionDescriptor.getArgs().get("replacementEdit"));
     }
 
-    public Map getDescriptorParams(Map velocityParams) {
+    public Map getDescriptorParams(Map velocityParams)
+    {
         Map<String, String> params = new HashMap<String, String>();
         String value = extractSingleParam(velocityParams, "fields");
         params.put("selectedField", value);
 
         value = extractSingleParam(velocityParams, "patternEdit");
         params.put("patternEdit", value);
+
+        value = extractSingleParam(velocityParams, "replacementEdit");
+        params.put("replacementEdit", value);
 
         return params;
     }
