3eadc9c471ec2d6793f7c347c5d31ebc03124069
ganzha.vitaliy <ganzha.vitaliy@b272e688-8835-11de-a4f2-b7219ab9ea91>
2009-09-07 20:46:49 +0000

    Added: Some changes

---
+++ jira-plugin-pack/src/com/gsoft/workflow/validators/DateCompareValidator.java
@@ -0,0 +1,18 @@
+package com.gsoft.workflow.validators;
+
+import com.googlecode.jsu.workflow.validator.GenericValidator;
+import com.opensymphony.workflow.InvalidInputException;
+import com.opensymphony.workflow.WorkflowException;
+
+/**
+ *
+ * @author Ganzha Vitaliy
+ */
+public class DateCompareValidator extends GenericValidator{
+
+    @Override
+    protected void validate() throws InvalidInputException, WorkflowException {
+        return;
+    }
+
+}
