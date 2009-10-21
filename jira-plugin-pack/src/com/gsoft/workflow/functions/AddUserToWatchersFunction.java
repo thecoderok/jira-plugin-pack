7935e11ac2ce8926d70b6e877227da6c49c66d6c
ganzha.vitaliy <ganzha.vitaliy@b272e688-8835-11de-a4f2-b7219ab9ea91>
2009-10-21 06:57:29 +0000

    Fixed: PF will be produce change history

--- jira-plugin-pack/src/com/gsoft/workflow/functions/AddUserToWatchersFunction.java
+++ jira-plugin-pack/src/com/gsoft/workflow/functions/AddUserToWatchersFunction.java
@@ -10,7 +10,6 @@ import com.opensymphony.workflow.FunctionProvider;
 import com.opensymphony.workflow.WorkflowException;
 import java.util.Map;
 import com.atlassian.jira.security.Permissions;
-import org.ofbiz.core.entity.GenericValue;
 /**
  *
  * @author Ganzha Vitaliy
